8fe298aeb808ae8ff1c8b447a8473dba6f662e67
MDReptile <MDReptile@users.noreply.github.com>
2025-11-26 23:56:05 -0500

    Fix sender timing estimate to match actual continuous audio generation (66s instead of 90s)

--- RadioDataApp/ViewModels/MainViewModel.Transmission.cs
+++ RadioDataApp/ViewModels/MainViewModel.Transmission.cs
@@ -189,9 +189,7 @@ namespace RadioDataApp.ViewModels
             long fileSize = fileInfo.Length;
             int packetCount = (int)Math.Ceiling(fileSize / 200.0) + 1;
 
-            double firstPacketTime = 13.5;
-            double otherPacketTime = 9.5;
-            double estimatedSeconds = firstPacketTime + (packetCount - 1) * otherPacketTime;
+            double estimatedSeconds = 1.2 + ((packetCount - 1) * 8.3) + 0.8;
 
             if (estimatedSeconds > 30)
             {
